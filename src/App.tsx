97986566d669b2948a554042656780f1ddd1a7f1
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-13 17:12:30 +0900

    router test修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './assets/css/App.css';
+import {BrowserRouter} from "react-router-dom";
+import routes from './router/routes';
 
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+          {routes}
+      </BrowserRouter>
     </div>
   );
 }
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,18 +2,15 @@ import React from "react";
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 import itemList from '../components/itemList';
-import App from "../App";
 
 export const Path = {
-    app: '/',
-    itemList: '/itemList'
+    itemList: '/'
 };
 
 const routes = (
     <Switch>
-        <Route exact path={Path.app} component={App}/>
         <Route exact path={Path.itemList} component={itemList}/>
-        <Redirect to={Path.app}/>
+        <Redirect to={Path.itemList}/>
     </Switch>
 );
 
