39caf0cad41ce05eebb407b474109dc658a9b283
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2021-01-13 20:03:58 +0900

    order.sliceのinitialState作成

--- src/store/slices/Domain/order.slice.ts
+++ src/store/slices/Domain/order.slice.ts
@@ -1,7 +1,28 @@
 import {createSlice} from "@reduxjs/toolkit";
+import {Order, Topping} from '../../../types/interfaces'
+
+type orderState = {
+    order: Order,
+    subTotalPrice: number
+}
+
+const initialState: orderState = {
+    order: {
+        orderId: undefined,
+        totalPrice: 0,
+        destinationAddress: '',
+        destinationName: '',
+        deliveryDate: undefined,
+        status: undefined,
+        orderItems:[],
+        destinationZipcode: '',
+        paymentMethod: ''
+    },
+    subTotalPrice: 0
+}
 
 export const orderSlice = createSlice({
     name: '',
-    initialState: {},
+    initialState: initialState,
     reducers: {}
 })
