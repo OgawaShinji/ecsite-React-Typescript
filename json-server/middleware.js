b255a674d82acb5e901003950e213440f74e140b
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-14 09:05:12 +0900

    create json-server

---
+++ json-server/middleware.js
@@ -0,0 +1,9 @@
+module.exports = function (req, res, next) {
+    if (req.method === 'POST' | req.method === 'PUT') {
+        req.url += '_' + req.method // POSTもしくはPUTの時はreq.urlの語尾に'_POST'(もしくは'_PUT')をつける
+        req.method = 'GET' // GETに偽装
+        console.log(req.headers['token'])
+        console.log(req.url)
+    }
+    next()
+}
