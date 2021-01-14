aaca3e5e9c0546ea500a71caec8a66cf5b7138a8
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2021-01-14 12:56:55 +0900

    fetchOrderItemsメソッドの作成

--- src/store/slices/Domain/order.slice.ts
+++ src/store/slices/Domain/order.slice.ts
@@ -1,5 +1,7 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {Order, Topping} from '../../../types/interfaces'
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {Order} from '../../../types/interfaces'
+import {RootState} from "../../index";
+import axios from "axios";
 
 type orderState = {
     order: Order,
@@ -12,17 +14,112 @@ const initialState: orderState = {
         totalPrice: 0,
         destinationAddress: '',
         destinationName: '',
-        deliveryDate: undefined,
+        orderDate: undefined,
+        deliveryTime: undefined,
         status: undefined,
-        orderItems:[],
+        orderItems: [],
         destinationZipcode: '',
         paymentMethod: ''
     },
     subTotalPrice: 0
 }
 
+// --------------------------------  async progress  -----------------------------------------------------
+
+/**
+ * カート一覧を取得するメソッド
+ * @return res: orderItem[]
+ */
+
+export const fetchOrderItems = createAsyncThunk(
+    'order/fetchOrderItems',
+    async () => {
+        const {data} = await axios.get(`http://localhost:3000/cart`, {
+            method: 'GET',
+            headers: {
+                token: localStorage.getItem("token")
+            }
+        })
+        return {data: data}
+    }
+)
+
+/**
+ * 商品をカートに入れる関数
+ *  @param order: Order
+ *  @return void
+ */
+
+export const postOrderItem = createAsyncThunk(
+    'order/postOrderItem',
+    async () => {
+
+    }
+)
+
+/**
+ * カートに存在する商品内容を更新する関数
+ * @param order: Order
+ * @return void
+ */
+
+export const updateOrderItem = createAsyncThunk(
+    'order/updateOrderItem',
+    async () => {
+
+    }
+)
+
+/**
+ * カートに存在する商品を削除する関数
+ * @param orderItemId: number
+ * @return void
+ */
+
+export const deleteOrderItem = createAsyncThunk(
+    'order/deleteOrderItem',
+    async () => {
+
+    }
+)
+
+/**
+ * カートに存在する商品を注文する関数
+ * @param order: Order
+ * @return void
+ */
+
+export const postOrder = createAsyncThunk(
+    'order/postOrder',
+    async () => {
+
+    }
+)
+
+
+// ------------------------------------  slice  ---------------------------------------------------
+
 export const orderSlice = createSlice({
     name: 'order',
     initialState: initialState,
-    reducers: {}
+    reducers: {
+        setOrderItems: ((state: orderState, action) => {
+            state.order = action.payload
+        }),
+        setOrderUserInfo: ((state, action) => {
+
+        }),
+        setOrder: ((state, action) => {
+
+        })
+    },
+    extraReducers: (builder => {
+        builder.addCase(fetchOrderItems.fulfilled, (state, action) => {
+            console.log(action.payload.data.order.orderItems)
+            const _action = orderSlice.actions.setOrderItems(action.payload.data.order)
+            orderSlice.caseReducers.setOrderItems(state, _action)
+        })
+    })
 })
+
+export const selectOrder = (state: RootState) => state.order.order.orderItems
