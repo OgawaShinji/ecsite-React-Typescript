908f45c535ac08d1b66a330120829a94fac30a0f
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2021-01-15 15:32:19 +0900

    cartの雛形

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,8 +1,11 @@
 import {FC} from "react";
+import CartList from "./CartList"
 
 const Cart: FC = () => {
     return (
-        <></>
+        <div>
+            <CartList/>
+        </div>
     )
 }
 export default Cart;
