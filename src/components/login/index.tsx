cea9d29ff3c65a9ecd2250e319ec2d3a5dfc7cca
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-15 15:29:21 +0900

    [login component] create loginForm.tsx

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,6 +1,26 @@
-import {FC} from "react";
+import React from "react"
+import LoginForm from "~/components/login/loginForm";
+import {Link} from 'react-router-dom'
+import {Path} from "~/router/routes";
 
-const Login: FC = () => {
-    return (<></>)
+const Login: React.FC = () => {
+    const style: { [key: string]: React.CSSProperties } = {
+        LoginForm: {
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            transform: 'translate(-50%, -50%)'
+        },
+        Link: {
+            position: 'absolute',
+            left: '50%',
+            top: '80%',
+            transform: 'translate(-50%, -50%)'
+        }
+    }
+    return (<>
+        <LoginForm styleProps={style.LoginForm}/>
+        <Link to={Path.register} style={style.Link}>ユーザー登録はこちらから</Link>
+    </>)
 }
 export default Login;
