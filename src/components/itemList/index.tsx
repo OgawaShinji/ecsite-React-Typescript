ca14b9736d23c507a78c99826c7cfb82be9bf164
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-18 16:02:32 +0900

    SearchAreaコンポーネントの表示

--- src/components/itemList/index.tsx
+++ src/components/itemList/index.tsx
@@ -1,31 +1,55 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import {useDispatch, useSelector} from "react-redux";
-import {fetchItems, fetchItemNames, selectItems, selectItemNames} from "~/store/slices/Domain/item.slice";
+import {fetchItems, selectItems} from "~/store/slices/Domain/item.slice";
+
+import SearchArea from "~/components/itemList/SearchArea";
+import {Grid} from "@material-ui/core";
+import OptionForm from "~/components/itemList/OptionForm";
 
 const ItemList: React.FC = () => {
 
     const dispatch = useDispatch();
 
-    // stateからデータを取得
+    // 表示件数
+    const displayItems = [
+        {
+            value: 9,
+            label: '9件'
+        },
+        {
+            value: 15,
+            label: '15件'
+        }
+    ];
+    const [displayCount, setDisplayCount] = useState(displayItems[0].value);
+
+    // storeからstateの取得
     const items = useSelector(selectItems);
-    const itemNames = useSelector(selectItemNames);
 
     useEffect(() => {
         dispatch(fetchItems({itemName: '', sortId: 0}));
-        dispatch(fetchItemNames());
-    }, [dispatch])
+    }, [dispatch]);
 
     return (
-        <>
+        <div style={{backgroundColor: 'rgb(211, 211, 211)'}}>
+            <Grid container justify={"center"} alignItems={"center"}>
+                <Grid item xs={8}>
+                    <SearchArea handleSearch={(searchForm) => {
+                        dispatch(fetchItems(searchForm))
+                    }}/>
+                </Grid>
+                <Grid item xs={2}>
+                    <OptionForm label={'表示件数'} value={displayCount} optionItems={displayItems} handleChange={(val) => {
+                        setDisplayCount(val);
+                    }}/>
+                </Grid>
+            </Grid>
+
             {items && items.map((item) => (
                 <div key={item.id}>Mサイズ価格：{item.priceM}</div>
             ))}
-
-            {itemNames && itemNames.map((itemName, index) => (
-                <div key={index}>{itemName}</div>
-            ))}
-        </>
+        </div>
     )
 }
 export default ItemList;
