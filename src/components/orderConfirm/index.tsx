6593a6197ee90670ae1609985ba5fda337cf03de
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-01-18 11:04:45 +0900

    TotalPriceコンポーネントへ小計の値を伝達

--- src/components/orderConfirm/index.tsx
+++ src/components/orderConfirm/index.tsx
@@ -1,11 +1,21 @@
-import React, {FC} from "react";
+import React from "react";
 import OrderItem from "~/components/elements/orderItem";
+import TotalPrice from "~/components/elements/totalPrice";
+import { selectOrderSubTotalPrice} from "~/store/slices/Domain/order.slice";
+import { useSelector} from "react-redux";
 
-const OrderConfirm: FC = () => {
+
+const OrderConfirm: React.FC = () => {
+    //注文内容の小計を取得
+    const orderSubTotalPrice = useSelector(selectOrderSubTotalPrice);
+
     return (
         <>
             <div>
                 <OrderItem/>
+            </div>
+            <div>
+                <TotalPrice subTotalPrice={orderSubTotalPrice}/>
             </div>
         </>
     )
