504c7f2c12c2eea72384cf590aa04ad00a6a3caa
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-01-19 12:47:37 +0900

    ・paginationコンポーネントを実装 ・ページング機能を実装

--- src/components/itemList/index.tsx
+++ src/components/itemList/index.tsx
@@ -3,16 +3,21 @@ import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchItems, selectItems} from "~/store/slices/Domain/item.slice";
 
-import SearchArea from "~/components/itemList/SearchArea";
 import {Grid, makeStyles} from "@material-ui/core";
+import {Pagination} from "@material-ui/lab";
+
+import SearchArea from "~/components/itemList/SearchArea";
 import OptionForm from "~/components/itemList/OptionForm";
 import ItemCard from "~/components/itemList/ItemCard";
 
 const useStyles = makeStyles((theme) => ({
     itemCard: {
         margin: theme.spacing(1)
+    },
+    pagination: {
+        margin: theme.spacing(3)
     }
-}))
+}));
 
 const ItemList: React.FC = () => {
 
@@ -26,27 +31,46 @@ const ItemList: React.FC = () => {
             label: '9件'
         },
         {
-            value: 15,
-            label: '15件'
+            value: 18,
+            label: '18件'
         }
     ];
-    const [displayCount, setDisplayCount] = useState(displayItems[0].value);
 
     // storeからstateの取得
     const items = useSelector(selectItems);
+
+    // コンポーネント上のstate
+    const [displayCount, setDisplayCount] = useState(displayItems[0].value);
+    const [pageCount, setPageCount] = useState(items.length / displayCount);
+    const [page, setPage] = useState(1);
 
     useEffect(() => {
         dispatch(fetchItems({itemName: '', sortId: 0}));
     }, [dispatch]);
 
+    useEffect(() => {
+        // 総ページ数の算出
+        let totalPageCount;
+        if (items.length % displayCount === 0) {
+            totalPageCount = items.length / displayCount;
+        } else {
+            totalPageCount = Math.floor((items.length / displayCount) + 1);
+        }
+        setPageCount(totalPageCount);
+        setPage(1);
+    }, [items, displayCount]);
+
     return (
-        <div style={{backgroundColor: 'rgb(211, 211, 211)'}}>
+        <div>
             <Grid container justify={"center"} alignItems={"center"}>
+                {/*検索フォーム*/}
                 <Grid item xs={8}>
                     <SearchArea handleSearch={(searchForm) => {
                         dispatch(fetchItems(searchForm))
                     }}/>
                 </Grid>
+
+                {/*表示件数切り替え*/}
                 <Grid item xs={2}>
                     <OptionForm label={'表示件数'} value={displayCount} optionItems={displayItems} handleChange={(val) => {
                         setDisplayCount(val);
@@ -54,11 +78,12 @@ const ItemList: React.FC = () => {
                 </Grid>
             </Grid>
 
+            {/*商品一覧*/}
             <Grid container justify={"center"} alignItems={"center"}>
                 <Grid item xs={10}>
                     <Grid container justify={"center"} alignItems={"center"}>
-                        {items && items.map((item) => (
-                            <Grid item xs={3} className={classes.itemCard}>
+                        {items && items.slice(displayCount * (page - 1), displayCount * page).map((item) => (
+                            <Grid key={item.id} item xs={3} className={classes.itemCard}>
                                 <ItemCard item={item}/>
                             </Grid>
                         ))}
@@ -66,7 +91,14 @@ const ItemList: React.FC = () => {
                 </Grid>
             </Grid>
 
+            {/*Pagination*/}
+            <Grid container justify={"center"} alignItems={"center"}>
+                <Pagination count={pageCount} page={page} onChange={(e, val) => {
+                    setPage(val)
+                }} className={classes.pagination} size={"large"}/>
+            </Grid>
         </div>
-    )
-}
+    );
+};
+
 export default ItemList;
