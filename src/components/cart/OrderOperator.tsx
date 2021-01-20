527a38b2f4f3c9ee1f4557ca718bab740e06fe55
yamagishikotaro <kotaro.yamagishi@rakus-partners.co.jp>
2021-01-20 14:56:30 +0900

    feat: カート一覧のレイアウトの実装、画面遷移処理の追加

--- src/components/cart/OrderOperator.tsx
+++ src/components/cart/OrderOperator.tsx
@@ -1,6 +1,8 @@
-import {FC} from "react";
+import React, {FC} from "react";
 import {Button, Card, CardActionArea, CardActions, CardContent, makeStyles} from "@material-ui/core";
 import {OrderItem} from "~/types/interfaces";
+import TotalPrice from "~/components/elements/totalPrice"
+import {RouteComponentProps, withRouter} from 'react-router-dom';
 
 interface Props {
     subTotalPrice: number
@@ -12,17 +14,24 @@ interface Props {
 const useStyles = makeStyles({
     root: {
         maxWidth: 345,
+        margin: 'auto'
     },
     media: {
         height: 140,
     },
     btn: {
         margin: 'auto'
+    },
+    orderBtn: {
+        margin: 'auto',
+        color: 'white',
+        backgroundColor: '#3cb371',
+        width: 200
     }
 });
 
 
-const OrderOperator: FC<Props> = (props) => {
+const OrderOperator: FC<Props & RouteComponentProps> = (props) => {
 
     const classes = useStyles();
 
@@ -41,29 +50,38 @@ const OrderOperator: FC<Props> = (props) => {
         <Card className={classes.root}>
             <CardActionArea>
                 <CardContent>
-                    合計金額：2000
+                    <TotalPrice subTotalPrice={props.subTotalPrice}/>
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary" className={classes.btn}>
+                <Button
+                    variant="outlined"
+                    className={classes.orderBtn}
+                    onClick={() => props.history.push({pathname: `/orderConfirm`})}
+                >
                     注文する
                 </Button>
             </CardActions>
             <CardActions className={classes.btn}>
                 {/*TODO: カートが空なら押せないようにしておく*/}
                 <Button
-                    size="small"
-                    color="primary"
+                    variant="outlined"
+                    color="secondary"
                     className={classes.btn}
                     onClick={() => allDeleteOrderItems(props.orderItems!)}
                 >
                     カートを空にする
                 </Button>
-                <Button size="small" color="primary" className={classes.btn}>
+                <Button
+                    variant="outlined"
+                    color="primary"
+                    className={classes.btn}
+                    onClick={() => props.history.push({pathname: `/itemList`})}
+                >
                     買い物を続ける
                 </Button>
             </CardActions>
         </Card>
     )
 }
-export default OrderOperator;
+export default withRouter(OrderOperator);
