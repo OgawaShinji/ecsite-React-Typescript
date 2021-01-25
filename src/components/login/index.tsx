fd65075bc2832ac8519a9fe22a79d6a0ea37e22a
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-25 23:04:34 +0900

    【ログイン】レイアウト修正

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -2,24 +2,33 @@ import React from "react"
 import LoginForm from "~/components/login/LoginForm";
 import {Link} from 'react-router-dom'
 import {Path} from "~/router/routes";
+import {createStyles, Grid} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 const Login: React.FC = () => {
-    const style: { [key: string]: React.CSSProperties } = {
+    const loginStyle = makeStyles(() => createStyles({
         LoginForm: {},
-        Link: {}
-    }
-    return (<>
-        <div style={{
-            justifyContent: "center",
-            alignItems: "center",
+        Link: {},
+        login_form: {
             textAlign: "center",
-            width: "50%",
-            margin: "0 auto",
-            transform:"translateY(50%)"
-        }}>
-            <LoginForm styleProps={style.LoginForm}/>
-            <Link to={Path.register} style={style.Link}>ユーザー登録はこちらから</Link>
-        </div>
-    </>)
+            width: "25%",
+            marginTop: "7%",
+            marginLeft: "37%",
+
+        }
+    }))
+    const classes = loginStyle();
+
+    return (
+        <Grid container justify={"center"} className={classes.login_form}>
+            <Grid item xs={12}>
+                <LoginForm/>
+            </Grid>
+            <Grid item xs={12} style={{paddingTop: "10%"}}>
+                <Link to={Path.register}>ユーザー登録はこちらから</Link>
+            </Grid>
+        </Grid>
+    )
 };
+
 export default Login;
