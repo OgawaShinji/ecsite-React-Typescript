c042fc1f8cbfb412cb55824ea997b4b1d3f8203c
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-25 09:01:36 +0900

    npm i graphql

--- src/components/itemDetail/index.tsx
+++ src/components/itemDetail/index.tsx
@@ -20,6 +20,36 @@ import {createStyles, makeStyles} from "@material-ui/core/styles";
 import {asyncPostOrderItem, OrderItemToPost} from "~/store/slices/Domain/order.slice";
 import {Path} from "~/router/routes";
 import {setError} from "~/store/slices/App/error.slice"
+import ApolloClient from "apollo-boost"
+import gql from "graphql-tag"
+import {REST_URL} from "~/store/api";
+
+const client = new ApolloClient({
+    uri: REST_URL + '/django_ql/',
+    request: operation => {
+        operation.setContext({
+            headers: {
+                Authorization: localStorage.getItem("Authorization")
+            }
+        });
+    }
+});
+const query = gql(`query {
+  items {
+    edges {
+      node {
+        id
+        name
+        description
+        priceM
+        priceL
+        imagePath
+        deleted
+      }
+    }
+  }
+}
+`)
 
 const ItemDetail: React.FC = () => {
     const item = useSelector(selectItemDetail)
@@ -27,6 +57,8 @@ const ItemDetail: React.FC = () => {
     const dispatch: AppDispatch = useDispatch()
     const history = useHistory();
     const [detail, setDetail] = useState<Item | null>(item);
+
+    client.query({query}).then(result => console.log(result)).catch((e) => console.log(e))
 
     let {itemId}: any = useParams()
     itemId = Number(itemId)
