df3996ddf5d358402c915ec781d171c5d2cd03d4
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-28 13:18:54 +0900

    create test dir

--- src/__tests__/components/item.test.tsx
+++ src/__tests__/components/item.test.tsx
@@ -4,11 +4,9 @@ import {render,screen} from "@testing-library/react"
 import ItemDetail from "~/components/itemDetail"
 import store from "~/store";
 import {Provider} from "react-redux";
-import {useHistory, useParams} from "react-router-dom"
 
 describe('<ItemDetail>', () => {
     test("商品詳細のレンダリング", () => {
-
         render(<Provider store={store}><ItemDetail/></Provider>)
         screen.debug();
         expect(true).toBe(true);
