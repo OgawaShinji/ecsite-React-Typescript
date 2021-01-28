afe55fb635ec2df7548e698166d783dae460e193
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-01-28 13:18:41 +0900

    create test dir

---
+++ src/__tests__/components/item.test.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import '@testing-library/jest-dom/extend-expect'
+import {render,screen} from "@testing-library/react"
+import ItemDetail from "~/components/itemDetail"
+import store from "~/store";
+import {Provider} from "react-redux";
+import {useHistory, useParams} from "react-router-dom"
+
+describe('<ItemDetail>', () => {
+    test("商品詳細のレンダリング", () => {
+
+        render(<Provider store={store}><ItemDetail/></Provider>)
+        screen.debug();
+        expect(true).toBe(true);
+
+    })
+})
