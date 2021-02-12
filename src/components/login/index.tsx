e0a8705088ddfc890b9a54ea16d5ad95c3b557d7
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2021-02-12 20:39:04 +0900

    LoginForm.tsxのstate管理を変更

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import LoginForm from "~/components/login/LoginForm";
-import {Link} from 'react-router-dom'
-import {Path} from "~/router/routes";
-import {createStyles, Grid, LinearProgress} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import { Link } from 'react-router-dom'
+import { Path } from "~/router/routes";
+import { createStyles, Grid, LinearProgress } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const Login: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -30,15 +30,15 @@ const Login: React.FC = () => {
     const classes = loginStyle();
 
     return (isLoading ?
-            <LinearProgress style={{width: "60%", marginTop: "20%", marginLeft: "20%"}}/>
-            : <Grid container justify={"center"} className={classes.login_form}>
-                <Grid item xs={12}>
-                    <LoginForm setIsLoading={(is) => handleSetIsLoading(is)}/>
-                </Grid>
-                <Grid item xs={12} style={{paddingTop: "10%"}}>
-                    <Link to={Path.register}>ユーザー登録はこちらから</Link>
-                </Grid>
+        <LinearProgress style={{ width: "60%", marginTop: "20%", marginLeft: "20%" }} />
+        : <Grid container justify={"center"} className={classes.login_form}>
+            <Grid item xs={12}>
+                <LoginForm setIsLoading={(is) => handleSetIsLoading(is)} />
             </Grid>
+            <Grid item xs={12} style={{ paddingTop: "10%" }}>
+                <Link to={Path.register}>ユーザー登録はこちらから</Link>
+            </Grid>
+        </Grid>
     )
 };
 
